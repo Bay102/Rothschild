7c9c1aa3aa9ca501ccff2fd15c3eb80bb0d0b1b6
Zak Bay <zacharybay@live.com>
2023-08-05 05:31:19 -0700

    navbar built

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,11 +1,59 @@
-import styles from './navbar.module.css'
-
+import { NavLink } from 'react-router-dom';
+import styles from './navbar.module.css';
 
 export const Navbar = () => {
-
-   return (
-      <nav className={styles.navContainer}>
-         
-      </nav>
-   )
-}
+  return (
+    <nav className={styles.navContainer}>
+      <ul className={styles.navList}>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            ABOUT
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            OUR WORK
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            GRANTS
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            GET INVOLVED
+          </NavLink>
+        </li>
+        <li>
+          {' '}
+          <NavLink to="/" className={styles.navLink}>
+            NEWS
+          </NavLink>
+        </li>
+        <li className={`${styles.pushRight} ${styles.navButton} ${styles.contact}`}>
+          {' '}
+          <NavLink to="/" className={`${styles.navLink} ${styles.black}`}>
+            CONTACT
+          </NavLink>
+        </li>
+        <li className={`${styles.navLink} ${styles.navButton} ${styles.donate}`}>
+          {' '}
+          <NavLink to="/" className={`${styles.navLink} ${styles.black}`}>
+            DONATE
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
