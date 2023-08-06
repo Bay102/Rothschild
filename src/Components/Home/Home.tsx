33d41916491f289a553944df8a71def2feee9fa3
Zak Bay <zacharybay@live.com>
2023-08-06 01:36:31 -0700

    page 1 intro

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,13 +1,21 @@
-import { Navbar } from '../Navbar/Navbar'
-import styles from './home.module.css'
+import { NavLink } from 'react-router-dom';
+import { Navbar } from '../Navbar/Navbar';
+import styles from './home.module.css';
+import navStyles from '../Navbar/navbar.module.css';
 
 export const Home = () => {
-
-   return (
-     <div className={styles.homeContainer}>
-      <div><Navbar /></div>
-      
-      <h1>THE ROTHSCHILD TRUST</h1>
-     </div>
-   )
-}
+  return (
+    <div className={styles.homeContainer}>
+      <Navbar />
+      <section className={styles.sectionOne}>
+        <h1>THE ROTHSCHILD TRUST</h1>
+        <h3>Creating Stronger Communities. Building a Better You</h3>
+        <div className={styles.learnMore}>
+          <NavLink to={'/'} className={navStyles.black}>
+            LEARN MORE
+          </NavLink>
+        </div>
+      </section>
+    </div>
+  );
+};
