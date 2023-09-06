f6293384fb821b2253d034ffaee7a5a2c22db63c
Zak Bay <zacharybay@live.com>
2023-09-05 23:33:21 -0700

    navbar orange

--- src/Components/Grants/Grants.tsx
+++ src/Components/Grants/Grants.tsx
@@ -1,15 +1,15 @@
-import '/src/App.css'
-import styles from './grants.module.css'
-import { Footer } from "../Footer/Footer"
-import { Navbar } from "../Navbar/Navbar"
-
+import '/src/App.css';
+import styles from './grants.module.css';
+import { Footer } from '../Footer/Footer';
+import { Navbar } from '../Navbar/Navbar';
 
 export const Grants = () => {
-
-   return (
-      <div className={styles.container}>
-         <Navbar />
-         <Footer />
-      </div>
-   )
-}
+  return (
+    <div className={styles.container}>
+      <section className={`${styles.heroImg} hero`}>
+        <Navbar />
+      </section>
+      <Footer />
+    </div>
+  );
+};
