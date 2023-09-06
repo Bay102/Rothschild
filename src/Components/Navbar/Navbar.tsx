de97558cd31724abb876dd3c5515e7427697d9ab
Zak Bay <zacharybay@live.com>
2023-09-05 18:41:24 -0700

    brown background

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,20 +4,8 @@ import { AiOutlineMenu } from 'react-icons/ai';
 // import { useState } from 'react';
 
 export const Navbar = () => {
-  // const [showAboutDropdown, setShowAboutDropdown] = useState(false);
-  // const [showInvolvedDropdown, setShowInvolvedDropdown] = useState(false);
-
-  // const toggleDropdown = (
-  //   dropdown: React.Dispatch<React.SetStateAction<boolean>>
-  // ) => {
-  //   dropdown(true);
-  //   setTimeout(() => {
-  //     dropdown(false);
-  //   }, 3500);
-  // };
-
   return (
-    <nav className={`${styles.navContainer} animate__animated animate__slideInLeft`}>
+    <nav className={`${styles.navContainer}`}>
       <ul className={styles.navList}>
         <div className={styles.logo}>THE ROTHSCHILD TRUST</div>
         <div className={styles.hamburger}>
@@ -32,27 +20,6 @@ export const Navbar = () => {
           <NavLink to="/about-us" className={styles.navLink}>
             ABOUT US
           </NavLink>
-          {/* {showAboutDropdown && (
-            <ul className={styles.dropDown}>
-              <li>
-                <NavLink to="/">OUR STORY</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">OUR VISION</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">CORE VALUES</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">WHO WE ARE</NavLink>
-              </li>
-            </ul>
-          )} */}
-        </li>
-        <li>
-          <NavLink to="/our-work" className={styles.navLink}>
-            OUR WORK
-          </NavLink>
         </li>
         <li>
           <NavLink to="/grants" className={styles.navLink}>
@@ -63,22 +30,6 @@ export const Navbar = () => {
           <NavLink to="/get-involved" className={styles.navLink}>
             GET INVOLVED
           </NavLink>
-          {/* {showInvolvedDropdown && (
-            <ul className={styles.dropDown}>
-              <li>
-                <NavLink to="/">SUPPORT US</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">MENTORING</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">JOIN OUR BOARD</NavLink>
-              </li>
-              <li>
-                <NavLink to="/">CAREERS</NavLink>
-              </li>
-            </ul>
-          )} */}
         </li>
         <li>
           <NavLink to="/news" className={styles.navLink}>
